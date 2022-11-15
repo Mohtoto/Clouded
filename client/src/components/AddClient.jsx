74fcb87e8aadcde2ea81e40e37cc89a5741c0949
Mohammad Totonchy <103252562+Mohtoto@users.noreply.github.com>
2022-11-15 23:25:18 +0800

    added the post function into the database

--- client/src/components/AddClient.jsx
+++ client/src/components/AddClient.jsx
@@ -5,83 +5,116 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 
+const initialState = {
+  name: "",
+  email: "",
+  contact: "",
+};
 
-
-const initialState = { 
-
-  name : '',
-  email : '',
-  contact:'',
-
-
-}
 const AddClient = () => {
-
   const [state, setstate] = useState(initialState);
+  const navigate = useNavigate()
 
+  const { name, email, contact } = state;
 
-  console.log(state);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setstate({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!name || !email || !contact){
+
+      toast.error('please Provide value into each input field')
+
+    }
+    else {
+
+      axios.post('http://localhost:8000/api/post' , {
+        name , email , contact
+      }).then(()=> {
+
+        setstate({ name : '' , email: '' , contact : ''})
+      }).catch((err)=> toast.error(err.response.data))
+
+      setTimeout(() => navigate('/MainPage'), 500);
+
+    }
+
+  };
+
   return (
     <>
       <Nav />
-      {/* absolute  top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 */}
-      <div className="row">
-        <form className="absolute  top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 ">
-          {/* ======================================================*/}
+    
 
-          <label
-            for="name"
-            className="max-w-lg	 block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-          >
-            <span className="text-xs font-medium text-gray-700"> Name </span>
+      <form
+        className="absolute  top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 "
+        onSubmit={handleSubmit}
+      >
+        {/* ======================================================*/}
 
-            <input
-              type="text"
-              id="name"
-              placeholder="John Doe"
-              className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-              value={name}
-              onChange={handleInputChange}
-            />
-          </label>
+        <label htmlFor="name">Name</label>
+          
 
-          {/* ======================================================*/}
+          <input
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            value={name}
+            onChange={handleInputChange}
+          />
+      
 
-          <label
-            for="email"
-            className="max-w-lg	 block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-          >
-            <span className="text-xs font-medium text-gray-700"> Email </span>
+        {/* ======================================================*/}
 
-            <input
-              type="email"
-              id="email"
-              placeholder="anthony@rhcp.com"
-              className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-              value={email}
-              onChange={handleInputChange}
-            />
-          </label>
+        <label htmlFor="email">email</label>
+        
 
-          {/* ======================================================*/}
-          <label
-            for="contact"
-            className="max-w-lg	 block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-          >
-            <span className="text-xs font-medium text-gray-700"> Contact </span>
+          <input
+            type="email"
+            name="email"
+            placeholder="anthony@rhcp.com"
+            className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            value={email}
+            onChange={handleInputChange}
+          />
 
-            <input
-              type="text"
-              id="contact"
-              placeholder="04270388779"
-              className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-              value={contact}
-              onChange={handleInputChange}
-            />
-          </label>
-          {/* ======================================================*/}
-        </form>
-      </div>
+        {/* ======================================================*/}
+     
+        <label htmlFor="contact">Contact</label>
+
+          <input
+            type="text"
+           
+           name="contact"
+            placeholder="04270388779"
+            className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+            value={contact}
+            onChange={handleInputChange}
+          />
+    
+        {/* ======================================================*/}
+
+        <input
+          type="submit"
+      
+          value="Save"
+          className=" w-full bg-[#40453e] text-white py-4 px-5  mx-2 border-none rounded-md cursor-none"
+        />
+
+        <Link to={'/MainPage'}>
+          <input
+            type="button"
+            value={"Go back"}
+            className=" w-full bg-[#40453e] text-white py-4 px-5  mx-2 border-none rounded-md cursor-none"
+          />
+        </Link>
+      </form>
     </>
   );
 };
