94a7ac950bb92e816aa51a6b09d2a8f71f28bfcc
Mohammad Totonchy <103252562+Mohtoto@users.noreply.github.com>
2022-12-05 02:56:39 +0800

    tweaked the table for responsiveness

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -28,7 +28,7 @@ const MainPage = () => {
   console.log(dataa);
 
   return (
-    <div className="flex w-full">
+    <div className="flex w-full min-h-screen">
       <Sidebar />
       <div className="row">
         <div className=" group-bubbles container mx-auto mt-12">
@@ -62,7 +62,46 @@ const MainPage = () => {
         
         </div>
 
-        <div className="table__info container flex justify-center mx-auto  mt-80">
+  <table className="min-w-full border-collapse block md:table">
+		<thead className="block md:table-header-group">
+			<tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+				<th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Name</th>
+				<th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Email</th>
+				<th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Contact</th>
+				<th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
+			</tr>
+		</thead>
+		<tbody className="block md:table-row-group">
+
+    {dataa.map((item, index) => (
+			<tr className="bg-gray-300 border border-grey-500 md:border-none block md:table-row" key={index}>
+				<td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Name</span>{item.name}</td>
+				<td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Email</span>{item.email}</td>
+				<td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 md:hidden font-bold">Contact</span>{item.contact}</td>
+				
+				<td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+					<span className="inline-block w-1/3 md:hidden font-bold">Actions</span>
+          <Link to={`/update/${item.id}`}>
+					    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+          </Link>
+					<button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded" onClick={()=>HandleDelete(item.id)}>Delete</button>
+
+          <Link to={'/view/'+item.id}>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">View</button>
+          </Link>
+
+				</td>
+			</tr>
+
+      ))}
+			
+		</tbody>
+	</table>
+
+
+
+{/* 
+        <div classNameName="table__info container flex justify-center mx-auto  mt-80">
           <div className="flex flex-col">
             <div className="w-full">
               <div className="border-b border-gray-200 shadow">
@@ -132,7 +171,7 @@ const MainPage = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
