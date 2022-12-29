73f85ed4945ef481782e91a11bb37a391a842705
Mohammad Totonchy <103252562+Mohtoto@users.noreply.github.com>
2022-12-29 19:24:20 +0800

    port added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 const mysql = require("mysql2");
 const { json } = require("express");
 const app = express();
