99b8b792dd0bf1771003840efee92e0d9ed5de06
Mohammad Totonchy <103252562+Mohtoto@users.noreply.github.com>
2022-12-29 12:53:50 +0800

    tweaked the main page

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -9,9 +9,6 @@ import logo from "../assets/avatar.svg";
 import { motion } from "framer-motion";
 import SidebarLists from "./SidebarLists";
 
-
-
-
 const MainPage = () => {
   const [dataa, setData] = useState([]);
   const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
@@ -37,7 +34,7 @@ const MainPage = () => {
   console.log(dataa);
 
   return (
-    <div className="flex w-full min-h-screen bg-[url('/src/assets/vec4.svg')] bg-no-repeat bg-contain bg-right">
+    <div className="flex min-h-screen w-full bg-[url('/src/assets/vec4.svg')] bg-contain bg-right bg-no-repeat">
       <Sidebar />
 
       <div className="row">
@@ -73,7 +70,7 @@ const MainPage = () => {
               </div>
               <ul
                 className={
-                  "MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]"
+                  "MENU-LINK-MOBILE-OPEN flex min-h-[250px] flex-col items-center justify-between"
                 }
               >
                 {/* <li>
@@ -106,7 +103,7 @@ const MainPage = () => {
                   opacity: 1,
                 }}
                 transition={{ duration: 0.5 }}
-                className="w-14 h-14"
+                className="h-14 w-14"
                 src={logo}
                 alt=""
               />
@@ -136,7 +133,7 @@ const MainPage = () => {
                 opacity: 1,
               }}
               transition={{ duration: 0.5 }}
-              className="lists flex gap-4 font-bold text-[#3C7699] text-lg"
+              className="lists flex gap-4 text-lg font-bold text-[#3C7699]"
             >
               <li>
                 <Link to="/">Home</Link>
@@ -151,23 +148,23 @@ const MainPage = () => {
           </div>
         </nav>
         <div className=" group-bubbles container mx-auto mt-12">
-          <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3 mx-4">
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-              <div className="text-sm font-medium text-gray-500 truncate">
+          <div className="mx-4 mb-6 grid grid-cols-1 gap-6 lg:grid-cols-3">
+            <div className="w-full rounded-lg bg-white px-4 py-5 shadow">
+              <div className="truncate text-sm font-medium text-gray-500">
                 Clients
               </div>
               <div className="mt-1 text-3xl font-semibold text-gray-900">3</div>
             </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow ">
-              <div className="text-sm font-medium text-gray-500 truncate">
+            <div className="w-full rounded-lg bg-white px-4 py-5 shadow ">
+              <div className="truncate text-sm font-medium text-gray-500">
                 Total Profit
               </div>
               <div className="mt-1 text-3xl font-semibold text-gray-900">
                 $ 450k
               </div>
             </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-              <div className="text-sm font-medium text-gray-500 truncate">
+            <div className="w-full rounded-lg bg-white px-4 py-5 shadow">
+              <div className="truncate text-sm font-medium text-gray-500">
                 Total Orders
               </div>
               <div className="mt-1 text-3xl font-semibold text-gray-900">
@@ -177,75 +174,75 @@ const MainPage = () => {
           </div>
         </div>
 
-        <table className="min-w-full border-collapse block md:table">
+        <table className="block min-w-full border-collapse md:table">
           <thead className="block md:table-header-group">
-            <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-              <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+            <tr className="border-grey-500 absolute -top-full -left-full block border md:relative md:top-auto md:left-auto md:table-row  md:border-none ">
+              <th className="md:border-grey-500 block bg-gray-600 p-2 text-left font-bold text-white md:table-cell md:border">
                 Name
               </th>
-              <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              <th className="md:border-grey-500 block bg-gray-600 p-2 text-left font-bold text-white md:table-cell md:border">
                 Email
               </th>
-              <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              <th className="md:border-grey-500 block bg-gray-600 p-2 text-left font-bold text-white md:table-cell md:border">
                 Contact
               </th>
-              <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              <th className="md:border-grey-500 block bg-gray-600 p-2 text-left font-bold text-white md:table-cell md:border">
                 Password
               </th>
-              <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">
+              <th className="md:border-grey-500 block bg-gray-600 p-2 text-left font-bold text-white md:table-cell md:border">
                 Actions
               </th>
             </tr>
           </thead>
-          <tbody className=" md:table-row-group table1">
+          <tbody className=" table1 md:table-row-group">
             {dataa.slice(0, 3).map((item, index) => (
               <tr
-                className="bg-gray-300 border border-grey-500 md:border-none  md:table-row p-5 flex flex-col"
+                className="mb-8 ml-3 mt-3 flex  flex-col rounded border border-stone-800 bg-gray-300 p-5 shadow-2xl md:table-row md:border-none"
                 key={index}
               >
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                  <span className="inline-block mr-3 md:hidden font-bold">
+                <td className="md:border-grey-500 block p-2 text-left md:table-cell md:border">
+                  <span className="mr-3 inline-block font-bold md:hidden">
                     Name
                   </span>
                   {item.name}
                 </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                  <span className="inline-block md:hidden font-bold mr-3">
+                <td className="md:border-grey-500 block p-2 text-left md:table-cell md:border">
+                  <span className="mr-3 inline-block font-bold md:hidden">
                     Email
                   </span>
                   {item.email}
                 </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                  <span className="inline-block mr-3 md:hidden font-bold">
+                <td className="md:border-grey-500 block p-2 text-left md:table-cell md:border">
+                  <span className="mr-3 inline-block font-bold md:hidden">
                     Contact
                   </span>
                   {item.contact}
                 </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                  <span className="inline-block mr-3 md:hidden font-bold">
+                <td className="md:border-grey-500 block p-2 text-left md:table-cell md:border">
+                  <span className="mr-3 inline-block font-bold md:hidden">
                     Password
                   </span>
-                  {item.contact}
+                  {item.password}
                 </td>
 
-                <td className="p-2 md:border md:border-grey-500 flex gap-1  md:table-cell">
-                  <span className="inline-block mr-3 md:hidden font-bold ">
+                <td className="md:border-grey-500 flex gap-1 p-2 md:table-cell  md:border">
+                  <span className="mr-3 inline-block font-bold md:hidden ">
                     Actions
                   </span>
                   <Link to={`/update/${item.id}`}>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                    <button className="rounded border border-blue-500 bg-blue-500 py-1 px-2 font-bold text-white hover:bg-blue-700">
                       Edit
                     </button>
                   </Link>
                   <button
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded"
+                    className="rounded border border-red-500 bg-red-500 py-1 px-2 font-bold text-white hover:bg-red-700"
                     onClick={() => HandleDelete(item.id)}
                   >
                     Delete
                   </button>
 
                   <Link to={"/view/" + item.id}>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
+                    <button className="rounded border border-blue-500 bg-blue-500 py-1 px-2 font-bold text-white hover:bg-blue-700">
                       View
                     </button>
                   </Link>
